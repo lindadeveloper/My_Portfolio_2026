3206af5f8487efb64f3ed188c8a18e851a210298
Linda Liu <lindacodesdev@gmail.com>
2026-02-13 23:08:57 -0800

    Building my portfolio
    Building my portfolio from scratch using React

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Linda Liu</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>Project #1</section>
+        <p>section description</p>
+      </main>
+      <footer>&copy;2026 by Linda Liu . All rights reserved</footer>
+    </>
+  );
+}
