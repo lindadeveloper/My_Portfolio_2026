30986f48fd8914686ba06e5151a95571500e3fb1
Linda Liu <lindacodesdev@gmail.com>
2026-02-28 09:18:39 -0800

    Fix images and added icons
    I fixed the profile and avatar images, because it wasn't rendering when I deployed the site. I also added icons from react-icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FaHtml5, FaCss3Alt, FaReact, FaNode, FaGithub, FaPython } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
 
 export default function Home() {
   return (
@@ -17,6 +19,21 @@ export default function Home() {
           My projects
         </Link>
       </div>
+      <h2>My Stacks</h2>
+      <FaHtml5 className="stack-icons" />
+      <span>HTML</span>
+      <FaCss3Alt className="stack-icons" />
+      <span>CSS</span>
+      <IoLogoJavascript className="stack-icons" />
+      <span>JavaScript</span>
+      <FaReact className="stack-icons" />
+      <span>React</span>
+      <FaPython className="stack-icons" />
+      <span>Python</span>
+      <FaNode className="stack-icons" />
+      <span>Node.js</span>
+      <FaGithub className="stack-icons" />
+      <span>Github</span>
     </section>
   );
 }
