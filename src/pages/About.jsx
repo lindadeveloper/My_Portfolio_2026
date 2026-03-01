77f5ca226068c9fe1c7eb21b7e2dd0e11612c0f2
Linda Liu <lindacodesdev@gmail.com>
2026-02-28 17:39:21 -0800

    Updated the tech stack icons CSS
    Updated the tech stack icons CSS

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,8 @@ export default function About() {
           I’m a lifelong learner who continuously improves her skills by staying consistent, being
           accountable, and self-motivated. I enjoy solving complex problems, optimizing performance,
           and exploring new technologies to build better products. My focus is on writing
-          maintainable code, designing scalable systems, and creating digital experiences that are
-          both intuitive and effective.
+          maintainable code as much as possible, designing scalable systems, and creating digital
+          experiences that are both intuitive and effective.
         </p>
         <p>
           When I’m not coding, you’ll probably find me walking in nature with my dog or trying out
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
-import { FaHtml5, FaCss3Alt, FaReact, FaNode, FaGithub, FaPython } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+  FaPython,
+  FaFigma,
+  FaUserCheck,
+  FaBootstrap,
+} from "react-icons/fa";
+import { RiJavascriptFill } from "react-icons/ri";
+import { DiPhotoshop, DiIllustrator } from "react-icons/di";
 
 export default function Home() {
   return (
     <section className="content-container">
       <h1 className="hero-h1">Hi, I'm Linda</h1>
       <p>
-        I'm a software developer who builds clean, user-friendly, and accessible web applications. I
-        focus on fast performance and simple, user-centered UX that helps turn visitors into happy
-        customers!
+        I'm a <span className="em-text">Front-End Developer</span> who builds user-friendly, and
+        accessible web applications. I focus on fast performance and simple, user-centered UX that
+        helps turn visitors into happy customers!
       </p>
       <div className="hero-btn-container">
         <Link to="about" className="hero-btn-about">
@@ -19,21 +30,63 @@ export default function Home() {
           My projects
         </Link>
       </div>
-      <h2>My Stacks</h2>
-      <FaHtml5 className="stack-icons" />
-      <span>HTML</span>
-      <FaCss3Alt className="stack-icons" />
-      <span>CSS</span>
-      <IoLogoJavascript className="stack-icons" />
-      <span>JavaScript</span>
-      <FaReact className="stack-icons" />
-      <span>React</span>
-      <FaPython className="stack-icons" />
-      <span>Python</span>
-      <FaNode className="stack-icons" />
-      <span>Node.js</span>
-      <FaGithub className="stack-icons" />
-      <span>Github</span>
+      <h2>Tech Stacks</h2>
+      <div className="icon-container">
+        <button className="icon-btn">
+          <FaHtml5 className="stack-icons" />
+          <span>HTML</span>
+        </button>
+        <button className="icon-btn">
+          <FaCss3Alt className="stack-icons" />
+          <span>CSS</span>
+        </button>
+        <button className="icon-btn">
+          <RiJavascriptFill className="stack-icons" />
+          <span>JavaScript</span>
+        </button>
+        <button className="icon-btn">
+          <FaReact className="stack-icons" />
+          <span>React</span>
+        </button>
+        <button className="icon-btn">
+          <FaNodeJs className="stack-icons" />
+          <span>Node.js</span>
+        </button>
+        <button className="icon-btn">
+          <FaPython className="stack-icons" />
+          <span>Python</span>
+        </button>
+        <button className="icon-btn">
+          <span>More to come...</span>
+        </button>
+      </div>
+      <h2>Tools</h2>
+      <div className="icon-container">
+        <button className="icon-btn">
+          <FaGithub className="stack-icons" />
+          <span>Github</span>
+        </button>
+        <button className="icon-btn">
+          <FaFigma className="stack-icons" />
+          <span>Figma</span>
+        </button>
+        <button className="icon-btn">
+          <FaUserCheck className="stack-icons" />
+          <span>UX / UI</span>
+        </button>
+        <button className="icon-btn">
+          <FaBootstrap className="stack-icons" />
+          <span>Bootstrap</span>
+        </button>
+        <button className="icon-btn">
+          <DiPhotoshop className="stack-icons" />
+          <span>Photoshop</span>
+        </button>
+        <button className="icon-btn">
+          <DiIllustrator className="stack-icons" />
+          <span>Illustrator</span>
+        </button>
+      </div>
     </section>
   );
 }
