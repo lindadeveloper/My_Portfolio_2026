b21a8646f809f5ddab0d116f9b453ba04844a1c9
Linda Liu <lindacodesdev@gmail.com>
2026-03-04 22:27:15 -0800

    Re-adjust the nav bar menu
    I re-adjusted the nav bar menu to fit the larger screen layout.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import ToggleTheme from "./ToggleTheme";
 import { Link, NavLink } from "react-router-dom";
 import { Menu, X, House, Presentation, CircleQuestionMark, GraduationCap } from "lucide-react";
 import { useState } from "react";
+import { CiCircleChevUp } from "react-icons/ci";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -27,40 +28,48 @@ export default function Header() {
             {hamburger}
           </button>
         </div>
-      </nav>
 
-      <nav className="nav-menu" style={toggleMenu}>
-        <ul>
-          <div className="menu-logo">
-            <img src="/assets/Linda-avatar.svg" />
-            <h1>Linda Liu</h1>
-          </div>
-          <li className="home">
-            <NavLink to="/" onClick={menuToggle} className="nav-link">
-              <House />
-              Home
-            </NavLink>
-          </li>
-          <li className="projects">
-            <NavLink to="projects" onClick={menuToggle} className="nav-link">
-              <Presentation />
-              Projects
-            </NavLink>
-          </li>
-          <li className="education">
-            <NavLink to="education" onClick={menuToggle} className="nav-link">
-              <GraduationCap />
-              Education
-            </NavLink>
-          </li>
-          <li className="about">
-            <NavLink to="about" onClick={menuToggle} className="nav-link">
-              <CircleQuestionMark />
-              About
-            </NavLink>
-          </li>
-        </ul>
+        <nav className="nav-menu" style={toggleMenu}>
+          <ul>
+            <div className="menu-logo">
+              <img src="/assets/Linda-avatar.svg" />
+              <h1>Linda Liu</h1>
+            </div>
+            <div className="nav-btn">
+              <li className="home">
+                <NavLink to="/" onClick={menuToggle} className="nav-link">
+                  <House />
+                  Home
+                </NavLink>
+              </li>
+              <li className="projects">
+                <NavLink to="projects" onClick={menuToggle} className="nav-link">
+                  <Presentation />
+                  Projects
+                </NavLink>
+              </li>
+              <li className="education">
+                <NavLink to="education" onClick={menuToggle} className="nav-link">
+                  <GraduationCap />
+                  Education
+                </NavLink>
+              </li>
+              <li className="about">
+                <NavLink to="about" onClick={menuToggle} className="nav-link">
+                  <CircleQuestionMark />
+                  About
+                </NavLink>
+              </li>
+            </div>
+          </ul>
+        </nav>
       </nav>
+      <button className="back-to-top-btn" aria-label="Back to top">
+        <a className="back-to-top-arrow" href="#top">
+          <CiCircleChevUp size={50} />
+        </a>
+        UP
+      </button>
     </header>
   );
 }
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import { CiCircleChevUp } from "react-icons/ci";
 
 export default function Layout() {
   return (
@@ -10,12 +9,6 @@ export default function Layout() {
       <main className="main-container">
         <Outlet />
       </main>
-      <button className="back-to-top-btn" aria-label="Back to top">
-        <a className="back-to-top-arrow" href="#top">
-          <CiCircleChevUp size={50} />
-        </a>
-        UP
-      </button>
       <Footer />
     </>
   );
