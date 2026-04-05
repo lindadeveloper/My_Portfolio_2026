2c4ff05d99e04381dd4169f3897e8f4c39af3890
Linda Liu <lindacodesdev@gmail.com>
2026-04-04 17:09:46 -0700

    Updated the budget app section
    Added a description of the project and a link for the budget app live demo

--- src/pages/Projects/BudgetApp.jsx
+++ src/pages/Projects/BudgetApp.jsx
@@ -13,13 +13,23 @@ export default function BudgetApp() {
           Live Demo
         </a>
         <span>|</span>
-        <a href="" alt="GitHub Link" className="live-demo-github">
+        <a
+          href="https://budget-app-beta-lemon.vercel.app/"
+          alt="GitHub Link"
+          className="live-demo-github"
+        >
           GitHub
         </a>
         <span>|</span>
         <p>A budget app to track all your expenses</p>
       </div>
-      <p className="p-default project-p">Currently work in progress...</p>
+      <p className="p-default project-p">
+        I designed and developed this budget app with a focus on intuitive UX/UI and user-friendly
+        experience. I initially created the design in Figma and conducted user research to better
+        understand how users interact with the app's layout. I then refined the design and added
+        features to improve usability and intuitiveness. I spent two weeks building this app using
+        only HTML, CSS, and JavaScript. This project is still a work in progress.
+      </p>
     </div>
   );
 }
