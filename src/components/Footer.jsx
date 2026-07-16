2cea6e7561438f8e36b8c173b3d31e774e8ceaab
Linda <lindacodesdev@gmail.com>
2026-07-16 11:06:15 -0700

    Updated the Footer
    Updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <footer className="footer">
-        &copy;2026 All rights reserved • Built by Linda Liu with React and Vite • Check out the
-        source over on
+        &copy;2026 All rights reserved • Designed & Built by Linda Liu with
+        React and Vite • Check out the source over on
         <a
           href="https://github.com/lindadeveloper/My_Portfolio_2026"
           target="_blank"
